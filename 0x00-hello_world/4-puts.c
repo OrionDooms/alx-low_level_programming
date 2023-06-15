c4bcdc875dff3821587a0185150915900f9818cb
OrionDooms <orion45.dd@gmail.com>
2023-06-15 14:44:34 -0700

    print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+
+/*printing using puts*/
 int main(void)
 {
-	char str1[] = {"Programming is like building a multilingual puzzle\0"};
+	char str1[] = {"\"Programming is like building a multilingual puzzle\0"};
 	puts(str1);
 	return 0;
 }
