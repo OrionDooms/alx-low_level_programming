045bfb4e7a568af04f654dfdf40134da33fa6570
OrionDooms <orion45.dd@gmail.com>
2023-06-16 10:57:07 -0700

    Intel

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-/*
- * main - program takes in a random number each time.
- * Description - then if the value is less it prints negative or greater
+
+/* betty style doc for function main goes there */
+/* main - program takes in a random number each time */
+/* Description - then if the value is less it prints negative or greater
  * prints positive and if it's zero it prints.
  * zero
  */
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
