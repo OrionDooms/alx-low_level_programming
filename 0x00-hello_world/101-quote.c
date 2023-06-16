53b380bc8cf368b29ed604b158567fbb81d9880e
OrionDooms <orion45.dd@gmail.com>
2023-06-16 14:11:36 -0700

    write function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ * description - This program uses the write function to output a message.
+ *
+ * Return: 1 (success)
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 62);
+	return (1);
+}
