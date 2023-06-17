851b67220a732b97813dbd229d5348628d600868
OrionDooms <orion45.dd@gmail.com>
2023-06-17 09:32:08 -0700

    number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description - This program uses printf to print out base 10
+ * starting from 0
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
