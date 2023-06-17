619cf34fc42e562771a2bc83db69663db550d60c
OrionDooms <orion45.dd@gmail.com>
2023-06-17 10:00:56 -0700

    0 to 9

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * description - This program uses putchar to print out numbers
+ * for 0 to 9.
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
 	int num;
