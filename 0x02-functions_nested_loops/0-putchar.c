de5f08a2fdb2547e66e9f69791425dc5c6d336f7
OrionDooms <orion45.dd@gmail.com>
2023-06-20 04:39:58 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description - This program print out _putchar
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
