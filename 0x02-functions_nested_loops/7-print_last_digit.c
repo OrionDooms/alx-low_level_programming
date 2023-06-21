f69d9b967d81a69c1cccae3c1e1a850259e997cd
OrionDooms <orion45.dd@gmail.com>
2023-06-21 14:41:57 -0700

    numbers

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,10 +13,6 @@ int print_last_digit(int i)
 		i = -i;
 	}
 	last = i % 10;
-	if (last < 0)
-	{
-		last = -1;
-	}
 	_putchar(last + '0');
 	return (last);
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,16 +1,27 @@
 #include "main.h"
 void times_table(void)
 {
-	int i, t;
+	int i, t, c;
 
 	i = '0';
 	t = '0';
-	while (i <= '9')
+	c = '0';
+	while (i <= '0')
 	{
-		_putchar(i);
-		_putchar(',');
-		_putchar(' ');
+		while (c <= '9')
+		{
+			while (t <= '9')
+			{
+				_putchar(t);
+				_putchar(',');
+				_putchar(' ');
+				t++;
+			}
+			_putchar('\n');
+			t = '0';
+			c++;
+		}
+		c = '0';
 		i++;
 	}
-	_putchar('\n');
 }
