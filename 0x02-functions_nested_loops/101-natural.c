13ea34592664e3d31f0f5885f642b156acdf4d3f
OrionDooms <orion45.dd@gmail.com>
2023-06-24 17:27:33 -0700

    3 or 5 from 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - prints multiples of 3 or 5 from 1024 that add everything up.
+ */
+void main(void)
+{
+	int i, c;
+
+	i = 1;
+	c = 0;
+	while (i < 1024)
+	{
+		if (i % 3 == 0)
+		{
+			printf("%d", i);
+			c = i + c;
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%d", i);
+			c = i + c;
+		}
+		i++;
+	}
+	printf("\n");
+	printf("%d", c);
+	printf("\n");
+}
