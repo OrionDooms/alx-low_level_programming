ae8862211839565feb1d64eeb1ada2d970913e1d
OrionDooms <orion45.dd@gmail.com>
2023-06-26 11:33:36 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+void print_rev(char *s)
+{
+	char x;
+	int i, a, b;
+
+	i = 0;
+	a = 0;
+	b = 0;
+
+	while (s[i] != '\0')
+	
+		i++;
+	}
+
+	a = i - 1 ;
+	while (b < i)
+	{
+		x[b] = s[a];
+		_putchar(x[b]);
+		a--;
+	}
+}
