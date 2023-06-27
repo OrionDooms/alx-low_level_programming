7709df0cef7eb6f4ec338890c2fc263c47f99ad0
OrionDooms <orion45.dd@gmail.com>
2023-06-26 17:09:13 -0700

    2:00 am

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,17 +5,16 @@
  */
 void rev_string(char *s)
 {
-	int a, i, b;
+	int a, i;
 
 	i = 0;
 	a = 0;
-	b = 0;
 	while (s[a] != '\0')
 	{
 		i = a;
 		a++;
 	}
-	while (s[i] > NULL)
+	while (s[i] > 0)
 	{
 		_putchar(s[i]);
 		i--;
