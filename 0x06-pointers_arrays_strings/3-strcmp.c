4c52806fc33f192bc3a97554cd2887e7b7821cbc
OrionDooms <orion45.dd@gmail.com>
2023-06-28 16:53:34 -0700

    uppercase

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,6 +13,8 @@ int _strcmp(char *s1, char *s2)
 	int i, b;
 
 	i = 0;
+	if (s1 == NULL || s2 == NULL)
+		return (0);
 	while (s1[i] != '\0')
 	{
 		while (s2[i] != '\0')
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+void reverse_array(int *a, int n)
+{
+	while (a[n] != '\0')
+	{
+		printf("%d",a[n]);
+		n++;
+	}
+}
