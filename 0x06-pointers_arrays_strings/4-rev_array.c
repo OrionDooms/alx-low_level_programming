78db32ae051027dd292a369fd6c93cde16df4d8c
OrionDooms <orion45.dd@gmail.com>
2023-06-29 04:02:20 -0700

    reverse_array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,10 +1,21 @@
 #include "main.h"
-
+/**
+ * reverse_array - reverses the content of an array of integers.
+ * @a: pointer of a array.
+ * @n: the amount of integers in a array.
+ */
 void reverse_array(int *a, int n)
 {
-	while (a[n] != '\0')
+	int i;
+	int s;
+
+	i = 0;
+	while (i < n)
 	{
-		printf("%d",a[n]);
-		n++;
+		s = a[i];
+		a[i] = a[n - 1];
+		a[n - 1] = s;
+		i++;
+		n--;
 	}
 }
