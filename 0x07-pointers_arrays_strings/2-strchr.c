6c26589dde8cd3bf8f5c5074325b1d49ea8168ae
OrionDooms <orion45.dd@gmail.com>
2023-07-03 06:37:55 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in string.
+ * @s: This is a pointer to a string.
+ * @c: Takes in a character.
+ * Return: A string from the character or NULL.
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
