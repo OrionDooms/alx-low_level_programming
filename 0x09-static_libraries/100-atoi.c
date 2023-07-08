932c1691176b054067298e0b5d95e6c27bbf03cf
OrionDooms <orion45.dd@gmail.com>
2023-07-07 19:13:28 -0700

    update

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,3 +1,24 @@
 #include "main.h"
+/**
+ * _atoi - takes a string and turns it in to integers.
+ * @s: Takes in a string.
+ * Return: return a integer.
+ */
+int _atoi(char *s)
+{
+	int n, i, a;
 
-int _atoi(char *s);
+	i = 0;
+	n = 0;
+	a = 0;
+	while (s[i])
+	{
+		if ((s[i] >= '0' && s[i] <= '9'))
+		{
+			a = (s[i] - '0');
+			n = n * 10 + a;
+			i++;
+		}
+	}
+	return (n);
+}
