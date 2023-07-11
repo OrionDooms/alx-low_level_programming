ec20f72c0ad4b759273f8bdffabe39a1faa2ad2e
OrionDooms <orion45.dd@gmail.com>
2023-07-11 12:44:30 -0700

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,16 +15,13 @@ char *str_concat(char *s1, char *s2)
 	for (a = 0; s2[a] != '\0'; a++)
 
 	z = malloc(sizeof(char) * (i + a));
-	for (e = 0; s1[e] != '\0'; e++)
+	if (z == NULL)
+		return (NULL);
+	for (e = 0; e < i; e++)
 	{
-		if (s1[e] == '\0')
-		{
-			z[e] = s1[e];
-		}
-		else
-			z[e] = s1[e];
+		z[e] = s1[e];
 	}
-	for (b = 0; s2[b] != '\0'; b++, e++)
+	for (b = 0; b < a; b++, e++)
 	{
 		z[e] = s2[b];
 	}
