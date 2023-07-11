ac5b07b8ece9493a2900a18f0b9b9d9f64986219
OrionDooms <orion45.dd@gmail.com>
2023-07-11 15:49:39 -0700

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,9 +11,14 @@ char *str_concat(char *s1, char *s2)
 	char *z;
 	int i, a, e, b;
 
-	for (i = 0; s1[i] != '\0'; i++)
-	for (a = 0; s2[a] != '\0'; a++)
-
+	if (s1 == NULL)
+		s1 = " ";
+	if (s2 == NULL)
+		s1 = " ";
+	while (*s1 != '\0')
+		i++, s1++;
+	while (*s2 != '\0')
+		a++, s2++;
 	z = malloc(sizeof(char) * (i + a));
 	if (z == NULL)
 		return (NULL);
@@ -27,7 +32,4 @@ char *str_concat(char *s1, char *s2)
 	}
 	return (z);
 	free(z);
-
-
-
 }
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -8,7 +8,7 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int i, **z; 
+	int i, **z;
 
 	if (width == 0 || height == 0)
 	{
