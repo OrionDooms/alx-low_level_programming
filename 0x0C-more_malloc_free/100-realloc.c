0aaacb1f8ffda5ed5b3f41ee460e5dd4ecf8831f
OrionDooms <orion45.dd@gmail.com>
2023-07-14 18:20:36 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,12 +13,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 
 	if (old_size == new_size)
-	{
 		return (ptr);
-	}
 	if (ptr == NULL)
 	{
-		return (malloc(new_size));
+		xptr = malloc(new_size);
+		return (xptr);
 	}
 	if (new_size == 0 && ptr != NULL)
 	{
@@ -26,6 +25,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 	xptr = malloc(new_size);
+	if (xptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
 	cpy = ptr;
 	if (new_size > old_size)
 	{
