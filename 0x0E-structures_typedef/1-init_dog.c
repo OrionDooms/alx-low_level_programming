0f2b2a2a7846e948d44c602e6c8395c0ca74bba3
OrionDooms <orion45.dd@gmail.com>
2023-07-17 15:26:59 -0700

    update

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,9 +7,11 @@
  * @name: name of dog.
  * @age: age of dog.
  * @owner: owner of dog.
- * */
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
