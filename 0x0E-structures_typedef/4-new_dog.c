24a27b6a921db0a98c0b77bce889c73b833e0965
OrionDooms <orion45.dd@gmail.com>
2023-07-17 16:51:23 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,7 +10,10 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *t;
+
 	t = malloc(sizeof(dog_t));
+	if (t == NULL)
+		return (NULL);
 	t->name = name;
 	t->age = age;
 	t->owner = owner;
