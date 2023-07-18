ff621d22a5165ddbd0b5fb42dd7f728b73c13f2f
OrionDooms <orion45.dd@gmail.com>
2023-07-18 15:07:41 -0700

    new

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,33 +12,36 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *t;
+	char *ndog, *odog;
 
 	if (name == NULL || owner == NULL || age < 0)
 		return (NULL);
+	ndog = malloc(sizeof(char) * (strlen(name + 1)));
+	if (ndog == NULL)
+	{
+		free(ndog);
+		return (NULL);
+	}
+	ndog = strdup(name);
+	odog = malloc(sizeof(char) * (strlen(owner + 1)));
+	if (odog == NULL)
+	{
+		free(ndog);
+		free(odog);
+		return (NULL);
+	}
+	odog = strdup(owner);
 	t = malloc(sizeof(dog_t));
 	if (t == NULL)
 	{
+		free(ndog);
+		free(odog);
 		free(t);
 		return (NULL);
 	}
-	t->name = malloc(sizeof(char) * (strlen(name + 1)));
-	if (t->name == NULL)
-	{
-		free(t->name);
-		free(t);
-		return (NULL);
-	}
-	t->owner = malloc(sizeof(char) * (strlen(owner + 1)));
-	if (t->owner == NULL)
-	{
-		free(t->name);
-		free(t->owner);
-		free(t);
-		return (NULL);
-	}
-	t->name = strcpy(t->name, name);
+	t->name = ndog;
 	t->age = age;
-	t->owner = strcpy(t->owner, owner);
+	t->owner = odog;
 	return (t);
 	free(t);
 }
