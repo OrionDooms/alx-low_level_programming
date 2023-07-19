f95cb91c2cb59f36980735ffe87a0094724062d8
OrionDooms <orion45.dd@gmail.com>
2023-07-19 06:50:15 -0700

    array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,12 +3,15 @@
 /**
  * array_iterator - given as a parameter on each element of an array.
  * @array: takes in array.
- * @size: size of the a array.
+ * @size: size of the array.
  * @action: a function that takes in a array.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
+
+	if (array == NULL || size == 0)
+		return;
 
 	while (i < size)
 	{
