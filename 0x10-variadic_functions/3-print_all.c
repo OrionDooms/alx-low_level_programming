d0ffff7176129dacea388d64594371f4c67055c2
OrionDooms <orion45.dd@gmail.com>
2023-07-21 10:33:32 -0700

    all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,21 +2,48 @@
 #include <stdarg.h>
 #include <stdio.h>
 /**
- * print_numbers - prints numbers, followed by a new line.
- * @separator: is the string to be printed between numbers.
- * @n:is the number of integers passed to the function.
+ * print_all - prints types of arguments, followed by a new line.
+ * @format: arguments passed to the function.
  */
 void print_all(const char * const format, ...)
 {
-        va_list ap;
-        unsigned int i;
+	va_list ap;
+	unsigned int i = 0;
+	char *str;
 
-        va_start(ap, format);
-        for (i = 0; i <= format; i++)
-        {
-                printf("%d", va_arg(ap, char));
-        }
-        printf("\n");
-        va_end(ap);
-
+	va_start(ap, format);
+	while (format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(ap, int));
+				if (format[i + 1] != '\0')
+					printf(", ");
+				break;
+			case 'i':
+				printf("%d", va_arg(ap, int));
+				if (format[i + 1] != '\0')
+					printf(", ");
+				break;
+			case 'f':
+				printf("%f", va_arg(ap, double));
+				if (format[i + 1] != '\0')
+					printf(", ");
+				break;
+			case 's':
+				str = va_arg(ap, char *);
+				if (str == NULL)
+					printf("(nil)");
+				printf("%s", str);
+				if (format[i + 1] != '\0')
+					printf(", ");
+				break;
+			default:
+				break;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(ap);
 }
