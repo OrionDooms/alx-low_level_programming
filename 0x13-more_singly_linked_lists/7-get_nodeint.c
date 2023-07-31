05746365fd9e7006215b6ac301bd968c44e9ae48
OrionDooms <orion45.dd@gmail.com>
2023-07-31 15:08:37 -0700

    sum

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -2,5 +2,17 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	listint_t *vice = head;
+        int i = 0, a = index;
 
+        while (vice != NULL)
+        {
+		if (i == a)
+		{
+			return (vice->n);
+		}
+		i++;
+                vice = vice->next;
+        }
+	return (NULL);
 }
