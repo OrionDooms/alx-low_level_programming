38892e7476e5f32ded73fcd5ccf717c45d3e6b1e
OrionDooms <orion45.dd@gmail.com>
2023-08-01 11:37:48 -0700

    betty check

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,12 +3,15 @@
 /**
  * add_nodeint - adds a new node at the beginning of a listint_t list.
  * @head: linked list.
- * Return: return a new added node
+ * @n: takes in a integer.
+ * Return: return a new added node.
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *vice;
 
+	if (*head == NULL)
+		return (NULL);
 	vice = malloc(sizeof(listint_t));
 	if (vice != NULL)
 	{
