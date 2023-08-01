58a17db84de1d7882a1e9fc26c782391bc90bc12
OrionDooms <orion45.dd@gmail.com>
2023-08-01 15:33:45 -0700

    delete node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,24 +1,50 @@
 #include "lists.h"
+/**
+ * delete_nodeint_at_index - deletes the node of a linked list.
+ * @head: linked list.
+ * @index: the position in a linked list.
+ * Return: (1) successful (-1) failed.
+ */
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *tmp, *vice;
-	int i = index;
+	int i, a = index;
 
-	tmp = *head;
-	if (tmp != NULL && tmp->n == i)
-	{
-		*head = tmp->next;
+	if (a < 0 || index > listint_len(*head))
 		return (-1);
-	}
-	while (tmp != NULL && tmp->n != i)
+	tmp = *head;
+	if (a == 0)
 	{
-		vice = tmp;
+		*head = (*head)->next;
+		tmp->next = NULL;
+		free(tmp);
+		return (1);
+	}
+	for (i = 0; i < a - 1; i++)
+	{
 		tmp = tmp->next;
 	}
-	if (tmp == NULL)
-		return (-1);
-	vice->next = tmp-> next;
+	vice = tmp->next;
+	tmp->next = tmp->next->next;
+	vice->next = NULL;
+	free(vice);
 	return (1);
-	free(tmp);
+
+}
+/**
+ * listint_len - count all the elements of a listint_t list.
+ * @h: linked list.
+ * Return: The number of nodes.
+ */
+size_t listint_len(const listint_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
