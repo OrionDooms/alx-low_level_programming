0e45c95f083645231b333fc5863fa03b87b68678
OrionDooms <orion45.dd@gmail.com>
2023-08-07 12:27:57 -0700

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	fp = open(filename, O_RDONLY | O_CREAT | O_TRUNC, 0600);
 	if (fp == -1)
 		return (-1);
-	if (text_content != NULL)
+	while (text_content != NULL)
 	{
 		i++;
 	}
