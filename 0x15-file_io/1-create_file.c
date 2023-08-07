fc2a1f5849619acb89079b43883c02d9007f201e
OrionDooms <orion45.dd@gmail.com>
2023-08-07 12:10:37 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fp = open(filename, O_RDONLY | O_CREAT | O_TRUNC);
+	fp = open(filename, O_RDONLY | O_CREAT | O_TRUNC, 0600);
 	if (fp == -1)
 		return (-1);
 	if (text_content != NULL)
