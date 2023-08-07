dc2a89d40c5c55594862823e7cdc163c4b577f63
OrionDooms <orion45.dd@gmail.com>
2023-08-07 12:00:40 -0700

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * create_file - create a new file.
+ * @filename: is the name of the file to create.
+ * @text_content: string to write to the file.
+ * Return:1 on success, -1 on failure.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fp, w, i = 0;
+
+	if (filename == NULL)
+		return (-1);
+	fp = open(filename, O_RDONLY | O_CREAT | O_TRUNC);
+	if (fp == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		i++;
+	}
+	w = write(fp, text_content, i);
+	if (w == -1)
+		return (-1);
+	close(fp);
+	return (1);
+
+}
