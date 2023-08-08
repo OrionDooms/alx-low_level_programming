cc6c3fa4d60ba809337f5708daf1a3f852d3596b
OrionDooms <orion45.dd@gmail.com>
2023-08-08 04:18:24 -0700

    task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,15 +5,11 @@
  * @text_content: string to write to the file.
  * Return:1 on success, -1 on failure.
  */
-
 int create_file(const char *filename, char *text_content)
 {
-	int fp, w, i = 0;
+	int fp, c, i = 0;
 
-	if (filename == NULL)
-		return (-1);
-	fp = open(filename, O_RDONLY | O_CREAT | O_TRUNC, 0600);
-	if (fp < 0)
+	if (filename == NULL || text_content == NULL)
 		return (-1);
 	if (text_content != NULL)
 	{
@@ -22,10 +18,12 @@ int create_file(const char *filename, char *text_content)
 			i++;
 		}
 	}
-	w = write(fp, text_content, i);
-	if (w < 0)
+	fp = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fp == -1)
+		return (-1);
+	c = write(fp, text_content, i);
+	if (c == -1)
 		return (-1);
 	close(fp);
 	return (1);
-
 }
