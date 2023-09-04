106429c09e49e2476718db7f60e975a7b35e93db
OrionDooms <orion45.dd@gmail.com>
2023-09-04 08:00:16 -0700

    append text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,6 +9,7 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int f, w, i;
 
+	i = 0;
 	if (filename == NULL)
 		return (-1);
 	f = open(filename, O_WRONLY | O_APPEND);
@@ -16,7 +17,6 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		i = 0;
 		while (text_content[i])
 			i++;
 	}
