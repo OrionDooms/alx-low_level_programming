d74cb83cce0325383933dd3fbf92c2a1f1c43783
OrionDooms <orion45.dd@gmail.com>
2023-10-26 06:12:39 -0700

    get bit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,8 +2,9 @@
 #define MAIN_H
 #include <stdio.h>
 
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
-int _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
 
 #endif
