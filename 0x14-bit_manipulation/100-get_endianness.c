dbff8f824675782a6fc36ca7a3bc2ecff0059430
OrionDooms <orion45.dd@gmail.com>
2023-10-27 09:33:51 -0700

    endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,5 +1,8 @@
 #include "main.h"
-
+/**
+ * get_endianness - checks the endianness.
+ * Return: return (0) if big endian or return (1) if little endian.
+ */
 int get_endianness(void)
 {
 	int i = 1;
