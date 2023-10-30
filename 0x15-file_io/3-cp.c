374834ca7b0d6ab1f21737faf79a69df3f258f86
OrionDooms <orion45.dd@gmail.com>
2023-10-30 12:37:45 -0700

    cp

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * main - copies the content of a file to another file.
+ * @ac: number of arguments.
+ * @av: Takes arguments multiplies.
+ * Return: 0 (success).
+ */
+int main(int argc, char **argv)
+{
+	int file_from, file_to, cp, cls1, cls2;
+	char *buffer = malloc(sizeof(char) * (1024));
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(98);
+	}
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1 || argv[1] == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	cp = read(file_from, buffer, 1024);
+	if (cp == -1 || file_to == -1 || argv[2] == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	write(file_to, buffer, cp);
+	cls1 = close(file_from);
+	cls2 = close(file_to);
+	if (cls1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	if (cls2)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
+	free(buffer);
+	return (0);
+}
