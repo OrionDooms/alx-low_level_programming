76b1827f0dd372ef25f900e3b5228f92d8c3b3ec
OrionDooms <orion45.dd@gmail.com>
2023-11-01 15:01:04 -0700

    update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+void close_file(int f);
 /**
  * main - copies the content of a file to another file.
  * @argc: number of arguments.
@@ -7,8 +8,9 @@
  */
 int main(int argc, char **argv)
 {
-	int file_from, file_to, cp, cls1, cls2;
-	char *buffer = malloc(sizeof(char) * (1024));
+	int file_from, file_to;/*, cls1, cls2;*/
+	ssize_t cp = 1024, file2;
+	char buf[1024];
 
 	if (argc != 3)
 	{
@@ -16,31 +18,45 @@ int main(int argc, char **argv)
 		exit(97);
 	}
 	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1 || argv[1] == NULL)
+	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	cp = read(file_from, buffer, 1024);
-	if (cp == -1 || file_to == -1 || argv[2] == NULL)
+	if (cp == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	write(file_to, buffer, cp);
-	cls1 = close(file_from);
-	cls2 = close(file_to);
-	if (cls1)
+	while (cp == 1024)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
+		cp = read(file_from, buf, 1024);
+		if (cp == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		file2 = write(file_to, buf, cp);
+		if (file2 == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
 	}
-	if (cls2)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-		exit(100);
-	}
-	free(buffer);
+	close_file(file_from);
+	close_file(file_to);
 	return (0);
+}
+
+void close_file(int f)
+{
+	int cls;
+
+	cls = close(f);
+	if (cls == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f);
+		exit(100);
+	}
 }
