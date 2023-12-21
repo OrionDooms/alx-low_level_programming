27a7640fb50a59c1dfaadfdfc01973485fe91e97
OrionDooms <orion45.dd@gmail.com>
2023-12-21 14:20:08 -0800

    creates a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,7 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (table == NULL || size == 0)
 		return (NULL);
 	table->size = size;
-	table->array = malloc(table->size, sizeof(hash_node_t));
+	table->array = malloc(sizeof(hash_node_t) * table->size);
 	if (table->array == NULL)
 		return (NULL);
 	while (i < table->size)
