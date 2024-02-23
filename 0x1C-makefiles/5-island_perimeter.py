a98a3fda41d9b2ce54fbebe7b112d47587e11cf9
OrionDooms <orion45.dd@gmail.com>
2024-02-23 07:36:45 -0800

    Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,9 +2,17 @@
 
 def island_perimeter(grid):
     result = 0
-    cell = 4
-    for i in range(len(grid)):
-        for j in range(len(grid)):
+    row = len(grid)
+    col = len(grid[0])
+    if row > 100 or col > 100:
+        return ("{}".format(result))
+    for i in range(row):
+        for j in range(col):
+            cell = 4
             if grid[i][j] == 1:
                 result += cell
+                if grid[i - 1][j] == 1:
+                    result -= 2
+                if grid[i][j - 1] == 1:
+                    result -= 2
     return ("{}".format(result))
