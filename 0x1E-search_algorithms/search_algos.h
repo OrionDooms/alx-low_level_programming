e199327e0757d519993ed1972ae4c8ed73545a4b
Orion <Orion45.dd@gmail.com>
2024-04-12 16:12:02 -0700

    Interpolation search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,10 +1,13 @@
 #ifndef _SEARCH_ALGOS_H
 #define _SEARCH_ALGOS_H
-
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+void display(int *array, int mid);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 
 #endif
